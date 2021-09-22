830f2b05ffcfd3f83fbea172d6d6e840faaa93b6
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2021-09-22 11:45:16 -0300

    Create XCode Project in SwiftUI

---
+++ Ingresso.com_Desafio_iOS/Ingresso_com_Desafio_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ingresso_com_Desafio_iOSApp.swift
+//  Ingresso.com_Desafio_iOS
+//
+//  Created by Gáudio Ney on 22/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Ingresso_com_Desafio_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
